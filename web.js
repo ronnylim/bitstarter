6678fa602cdfaead1394e51d5086983e9b90abcc
Ronny Lim <ronny.lim@rutgers.edu>
2013-07-10 17:54:09 +0000

    test ronny2

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(fs.readFileSync(index.html));
+//var buf = new Buffer(fs.readFileSync(index.html));
 
 app.get('/', function(request, response) {
   response.send("Ronny");
